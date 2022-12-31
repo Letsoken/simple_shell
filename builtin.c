4ed1a46ef42901ef126a7b4b46270d287dd396c2
Elite-AJ <59597378+Elite-AJ@users.noreply.github.com>
2022-12-31 18:32:16 +0100

    Update builtin.c

--- builtin.c
+++ builtin.c
@@ -47,7 +47,7 @@ int _mycd(info_t *info)
 		_puts("TODO: >>getcwd failure emsg here<<\n");
 	if (!info->argv[1])
 	{
-		dir = _getenv(info, "HOME=");
+		dir = _getenv(info_t, "HOME=");
 		if (!dir)
 			chdir_ret =
 				chdir((dir = _getenv(info, "PWD=")) ? dir : "/");
