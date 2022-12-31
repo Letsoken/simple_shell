acc53dff20fce16b008efcc106164991bee07b58
Elite-AJ <59597378+Elite-AJ@users.noreply.github.com>
2022-12-31 18:23:34 +0100

    Update builtin1.c

--- builtin1.c
+++ builtin1.c
@@ -109,7 +109,7 @@ int  _myalias(info_t *info)
 		if (p)
 			set_alias(info, info->argv[i]);
 		else
-			print_alias(mode_starts_with(info->alias, info->argv[i], '='));
+			print_alias(node_starts_with(info->alias, info->argv[i], '='));
 	}
 
 	return (0);
